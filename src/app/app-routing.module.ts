4a73a3a7a86c4f27352838fb116bca219ac01077
KendeAndras <totalrecord444@gmail.com>
2023-02-07 18:15:17 +0100

    componentek_mukodnek

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BmiComponent } from './bmi/bmi.component';
+import { AboutComponent } from './about/about.component';
+import { GithubComponent } from './github/github.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bmi', component: BmiComponent},
+  {path: 'github', component: GithubComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BmiComponent } from './bmi/bmi.component';
+import { AboutComponent } from './about/about.component';
+import { GithubComponent } from './github/github.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BmiComponent,
+    AboutComponent,
+    GithubComponent
   ],
   imports: [
     BrowserModule,
